5478d5ffab978356093f5374b253db2ce8a6bf68
Matt Dudzik <mdudzik92@gmail.com>
2020-11-12 12:00:23 -0600

    Imported employee list for viewing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    employees: [],
+    filteredEmployees: []
+  }
+  componentDidMount() {
+    fetch ('https://randomuser.me/api/?results=20').then(res => res.json()).then(res => {
+      this.setState({employees: res.results, filteredEmployees: res.results})
+    });
+  }
+  handleChange(event){
+    console.log(event.target.value)
+    const filteredEmployees = this.state.employees.filter(employee => employee.name.first.toLowerCase().includes(event.target.value.toLowerCase()) || employee.name.last.toLowerCase().includes(event.target.value.toLowerCase()))
+    this.setState({filteredEmployees})
+  }
+  render() {
+    return (
+      <div className="App">
+        <input type="text" onChange = {this.handleChange.bind(this)}/>
+        <table>
+          <thead>
+            <tr>
+              <td>First name</td>
+              <td>Last name</td>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.filteredEmployees.map(employee => {
+              return(
+                <tr>
+                  <td>{employee.name.first}</td>
+                  <td>{employee.name.last}</td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 }
 
 export default App;
